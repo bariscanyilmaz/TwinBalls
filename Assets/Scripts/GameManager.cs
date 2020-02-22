b847c5966bd3a1b28aec559c27bea9faa56f2d83
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2020-02-22 17:25:54 +0300

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    private int score;
+    public int Score{get{return score;}}
+    public TextMeshProUGUI scoreText;
+    private static GameManager _gameManager;
+
+    public static GameManager gameManager { get { return _gameManager; } }
+
+    void Awake()
+    {
+
+        if (_gameManager == null)
+        {
+            _gameManager = this;
+        }
+
+        score=PlayerPrefs.GetInt("score");
+    }
+
+
+    public void IncreaseScore(int score)
+    {
+        this.score+=score;
+    }
+
+    private void FixedUpdate()
+    {
+        scoreText.text="Score:"+score;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale=0;
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float verticalVelocity = 5;
+    public float horizontalVelocity = 0;
+    public Rigidbody2D otherPlayer;
+    public Collider2D ignoreCollider;
+    private Collider2D ownCollider;
+    private Rigidbody2D rigidBody;
+    private GameManager gameManager;
+    private bool isSuperPower;
+    private bool superPowerStarted;
+    private float duration;
+    public ParticleSystem particle;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        ownCollider = GetComponent<CircleCollider2D>();
+        Physics2D.IgnoreCollision(ignoreCollider, ownCollider);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameManager.Score % 500 == 0)
+        {
+
+            if (!superPowerStarted)
+            {
+                var offset = Mathf.Abs((otherPlayer.transform.position.x - transform.position.x));
+                if (offset <= (ownCollider.bounds.extents.x) * 2)
+                {
+                    isSuperPower = true;
+                    superPowerStarted = true;
+                }
+            }
+
+            if (duration < 2f)
+            {
+                if (isSuperPower)
+                {
+                    transform.position = new Vector3((transform.position.x + otherPlayer.position.x) / 2, transform.position.y, transform.position.z);
+                    rigidBody.velocity = new Vector2(0, verticalVelocity);
+                    duration += Time.deltaTime;
+
+                    Invoke("ResetDuration", 15);
+                }
+                else
+                {
+                    rigidBody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+                }
+            }
+            else
+            {
+                superPowerStarted = false;
+                rigidBody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+            }
+
+
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            horizontalVelocity = -(horizontalVelocity);
+            rigidBody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+        }
+
+    }
+
+
+    private void ResetDuration()
+    {
+        duration = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag == "Bonus")
+        {
+            if (collision.collider.gameObject.GetComponent<SpriteRenderer>().color == gameObject.GetComponent<SpriteRenderer>().color)
+            {
+
+                collision.collider.gameObject.transform.position = new Vector3(-collision.collider.gameObject.transform.position.x, collision.collider.gameObject.transform.position.y + 15, collision.collider.gameObject.transform.position.z);
+                gameManager.IncreaseScore(10);
+                rigidBody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+
+                particle.Play();
+            }
+            else
+            {
+                gameManager.RestartGame();
+            }
+        }
+
+
+
+        if (collision.collider.tag == "Trape")
+        {
+            collision.collider.gameObject.GetComponent<ParticleScript>().ParticalPlay();
+            gameManager.GameOver();
+
+        }
+    }
+
+
+}
